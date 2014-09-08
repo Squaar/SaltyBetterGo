75a28f61f913f2d36f034090a8fa7fcf29d7e005
Matt Dumford <mdumford99@gmail.com>
2014-09-08 01:36:20 -0500

    tournament wallet balance, get status

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	// "github.com/murz/go-socket.io"
+	// "net/http"
 )
 
 func main() {
@@ -10,5 +12,41 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(WalletBalance(client))
+	balance, err := WalletBalance(client)
+	if err != nil {
+		panic(err)
+	}
+
+	state, err := GetState(client)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Wallet Balance: %s", balance)
+	fmt.Println("State: %s", state)
+
+	// An attempt at creating a socket.io client...
+	// I don't know how to open a conection to the saltybet server
+	// I think the messages it sends are empty anyway. Can just check status periodically instead.
+
+	// sio := socketio.NewServer(nil)
+	// var msg socketio.Message
+	// http.Handle("/socket.io/", http.StripPrefix("/socket.io",
+	// 	sio.Handler(func(conn *socketio.Conn) {
+	// 		for {
+	// 			if err = conn.Receive(&msg); err != nil {
+	// 				panic(err)
+	// 			}
+	// 			switch msg.Type() {
+	// 			case socketio.MessageJSON, socketio.MessageText:
+	// 				fmt.Println("normal message: %s", msg.String())
+	// 			case socketio.MessageEvent:
+	// 				fmt.Println("event message: %s", msg.String())
+	// 			}
+	// 		}
+	// 	})))
+
+	// if err = http.ListenAndServe("www-cdn-twitch.saltybet.com:8000", nil); err != nil { // BROKEN
+	// 	panic(err)
+	// }
 }
--- web.go
+++ web.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"errors"
 	"io/ioutil"
 	"net/http"
 	"net/url"
@@ -33,11 +34,12 @@ func AuthClient(email, pass string) (*http.Client, error) {
 	if err != nil {
 		return nil, err
 	}
+	if resp.StatusCode != 200 && resp.StatusCode != 302 {
+		return nil, errors.New("Bad http response code.")
+	}
 
-	fmt.Println(resp.Status)
 	return client, nil
 }
-
 func WalletBalance(client *http.Client) (int, error) {
 	resp, err := client.Get("http://saltybet.com/ajax_tournament_end.php")
 	if err != nil {
@@ -50,4 +52,46 @@ func WalletBalance(client *http.Client) (int, error) {
 	}
 	balance, _ := strconv.Atoi(string(b))
 	return balance, nil
+}
+
+func TournamentBalance(client *http.Client) (int, error) {
+	resp, err := client.Get("http://saltybet.com/ajax_tournament_start.php")
+	if err != nil {
+		return 0, err
+	}
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return 0, err
+	}
+	balance, _ := strconv.Atoi(string(b))
+	return balance, nil
+}
+
+type SaltyState struct {
+	P1name    string
+	P2name    string
+	P1total   string
+	P2total   string
+	Status    string
+	Alert     string
+	X         int
+	Remaining string
+}
+
+func GetState(client *http.Client) (SaltyState, error) {
+	var state SaltyState
+	resp, err := client.Get("http://saltybet.com/state.json")
+	if err != nil {
+		return state, err
+	}
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return state, err
+	}
+	err = json.Unmarshal(b, &state)
+	if err != nil {
+		return state, err
+	}
+	return state, nil
 }
